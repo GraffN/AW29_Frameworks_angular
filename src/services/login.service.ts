1be68d720230ec842fc3e31ad3d3e18b81d4b474
eMpoWer <jorgen.graff-n@outlook.com>
2019-03-24 13:26:58 +0100

    added login and login service

---
+++ src/services/login.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+  userDetails: Object ={
+    username:localStorage.getItem('Username'),
+    password:localStorage.getItem('Password')
+  }
+
+  constructor() { }
+  getLoginDetails(){
+    return this.userDetails;
+  }
+}
